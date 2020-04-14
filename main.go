0f059aa7744d059b2978d4aa78d827b052938c60
bortnyakvova <bortnyakvova@gmail.com>
2020-04-15 00:20:41 +0300

    play with golang

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"os"
+	"strings"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
-}
-
 func main() {
-  http.HandleFunc("/", handlerFunc)
-  http.ListenAndServe(":3000", nil)
+	fmt.Println(os.Args[1])
+	fmt.Println(os.Args[2:])
+
+	for i := 0; i < len(os.Args); i++ {
+		fmt.Printf("index %d", i)
+		fmt.Printf(" element %s\n", os.Args[i])
+	}
+
+	var args = strings.Join(os.Args, " ")
+	fmt.Printf(args)
+	var s = strings.ToUpper(args)
+	fmt.Printf(s)
+
 }
